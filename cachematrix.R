4ddf1dddaf5f988dcdd89155415226b5098fdb87
KFoelFord <54045405+KFoelFord@users.noreply.github.com>
2019-08-12 19:04:02 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
 ## Write a short comment describing this function
+## ---------------------------------------------------------------------------------------------
+## For a given (not singular) matrix A, a special list/vector is created, consisting of the functions set, get, setAinv, getAinv
+## ---------------------------------------------------------------------------------------------
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(A = matrix()) {
+        Ainv <- NULL  
+        set <- function(y) {
+                A <<- y 
+                Ainv <<- NULL
+        }
+        get <- function() A  
+        setAinv <- function(solv) Ainv <<- solv  
+        getAinv <- function() Ainv  
+        list(set = set, get = get,
+             setAinv = setAinv,
+             getAinv = getAinv) 
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Write a short comment describing this function
+## ---------------------------------------------------------------------------------------------
+## For a given (not singular) matrix A, the inverse Ainv is calculated and cached - unless it was already calculated
+## ---------------------------------------------------------------------------------------------
+
+cacheSolve <- function(A=matrix(), ...) {
+        Ainv <- A$getAinv()
+        if(!is.null(Ainv)) {
+                message("getting cached data")
+                return(Ainv)
+        }
+        data <- A$get()
+        Ainv <- solve(data, ...)
+        A$setAinv(Ainv)
+        Ainv
 }
